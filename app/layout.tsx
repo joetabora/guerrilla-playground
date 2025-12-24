834923455597dbfcb14a2a63c5a704a2a202232a
Joseph Tabora <joetabora@gmail.com>
2025-12-24 16:25:46 -0600

    Add Google Analytics tracking (G-TJL4BVVSL1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter, JetBrains_Mono, Playfair_Display } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { siteConfig } from '@/lib/config/site'
 import { generateMetadata as generateSEOMetadata } from '@/lib/seo'
@@ -38,6 +39,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${jetbrains.variable} ${playfair.variable}`}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TJL4BVVSL1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TJL4BVVSL1');
+          `}
+        </Script>
+      </head>
       <body className="font-sans antialiased grain min-h-screen">
         <SiteHeader />
         <div className="relative min-h-screen flex flex-col">
